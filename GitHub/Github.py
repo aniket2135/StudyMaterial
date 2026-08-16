8e846c66b0a75418f414a179e97fb49de74a7d57
aniket2135 <aniketpatil2135@gmail.com>
2026-08-16 17:42:44 +0530

    Added github commandas

---
+++ GitHub/Github.py
@@ -0,0 +1,38 @@
+'''
+git clone https://github.com/aniket2135/StudyMaterial.git
+cd /home/aniket/Videos/Github/GitHub/StudyMaterial
+git config user.name "USERNAME"
+git config user.email "EMAIL"
+git config --get user.name
+git config --get user.email
+
+
+mkdir -p StudyMaterial/Python StudyMaterial/SQL
+mkdir -p StudyMaterial/FastApi
+mkdir -p StudyMaterial/Docker StudyMaterial/GitHub
+mkdir -p StudyMaterial/GenAI
+
+
+cd /home/aniket/Videos/Github/GitHub/StudyMaterial
+git status
+git add .
+git commit -m "Add study material folders"
+git push origin main
+
+
+
+Common daily Git commands
+
+git status
+git add file_name
+git add .
+git commit -m "Your message"
+git push origin main
+git pull origin main
+git checkout -b new-branch-name
+git checkout main
+git branch
+git log --oneline
+
+
+'''
