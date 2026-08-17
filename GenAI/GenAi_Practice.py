e15831ee8fce0cf66bdb42fdedfb96e5483e6396
aniket2135 <aniketpatil2135@gmail.com>
2026-08-17 09:16:27 +0530

    commit

--- GenAI/GenAi_Practice.py
+++ GenAI/GenAi_Practice.py
@@ -1,1 +1,19 @@
-#test
+from ollama import chat
+
+response = chat(
+    model="qwen2.5-coder:3b",
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain what an API is."
+        }
+    ]
+)
+
+print(response["message"]["content"])
+
+'''
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("BAAI/bge-small-en-v1.5")
+'''
